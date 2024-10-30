09e5e4d2829e1d5e9da33eddc6b071fe74902087
basednai <naiym.harris@icloud.com>
2024-10-29 22:01:20 -0400

    set defaults

--- src/getIcon.js
+++
@@ -1,5 +0,0 @@
-const image = document.querySelector("#iconImage")
-
-export function getIcon(iconName) {
-    image.src = `https://raw.githubusercontent.com/visualcrossing/WeatherIcons/refs/heads/main/SVG/4th%20Set%20-%20Color/${iconName}.svg`
-}
